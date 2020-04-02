1f633f3b93449266e83fb0ca37eea8cac7d12fcc
administrador <administrador@localhost.localdomain>
2020-04-02 15:10:52 -0400

    minor changes in logic

--- scripts/backupscript.sh
+++ scripts/backupscript.sh
@@ -11,15 +11,19 @@
 # in order to issue rsync this script mount windows shared folder de origem e o windows shared folder de destino
 # so, runs rsync in the mounted origem to mounted de destino
 
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+echo "[Backup] Info: Workdir = $(pwd)"
+
 nomeDaTarefa=$1
 if [ "X$nomeDaTarefa" == "X" ];
 then
-    echo "[ERRO]Passe o nome da tarefa como primeiro parametro, veja arquivo tarefas.txt"
+    echo "[BACKUP] Err: Passe o nome da tarefa como primeiro parametro, veja arquivo tarefas.txt"
     exit 1
 fi
 
 nomeDaTarefaFounded=-1
-basePathToMountOrigem=/home/wagner
+basePathToMount_CIFS_Origem=/home/administrador/mnt
 
 
 # Esta funcao busca pela linha referente ao nome da tarefa recebida no parametro deste script
@@ -30,24 +34,34 @@ basePathToMountOrigem=/home/wagner
 # lTaskDestino
 # lTaskVer
 function getTaskLineByTaskName(){
+	echo "[BACKUP] Debug: function getTaskLineByTaskName()..."
+	taskNameToSearchFor=$1
+	if [ "X$taskNameToSearchFor" == "X" ]; then
+		echo "[BACKUP] Debug: taskNameToSearchFor nao informada" 
+		return;
+	fi
+	echo "[BACKUP Debug: searching for $taskNameToSearchFor"
+	while read line; do
+        	IFS=';'
+        	read -ra l <<< $line;
+        	lTaskName=$(echo ${l[0]} | xargs) #trim l[0] string
+		echo ".[BACKUP] lTaskName=$lTaskName is $taskNameToSearchFor?"
+		if [ "$taskNameToSearchFor" == "$lTaskName" ];
+        		then
+ 				echo "..[BACKUP] Debug: Yep $taskNameToSearchFor == $lTaskName"
+            			
+				nomeDaTarefaFounded="1"            			
+	    			echo "..[BACKUP] Debug: Setting nomeDaTarefaFound to 1"
 
-    while read line; do
-        IFS=';'
-        read -ra l <<< $line;
-        lTaskName=$(echo ${l[0]} | xargs) #trim l[0] string
+				lTaskOrigem=$(echo ${l[1]} | xargs)
+				echo "..[BACKUP] Debug: Setting lTaskOrigem to $lTaskOrigem"
 
-        if [ "$nomeDaTarefa" = "$lTaskName" ];
-        then
-            nomeDaTarefaFounded="1"
-            lTaskNameSelected=$(echo ${l[0]} | xargs)
-            lTaskOrigem=$(echo ${l[1]} | xargs)
-            lTaskDestino=$(echo ${l[2]} | xargs)
-            lTaskVers=$(echo ${l[3]} | xargs)
-        fi
-        
-    done < ../tarefas-salvas/tarefas.txt
-
-    IFS=';'
+            			lTaskVers=$(echo ${l[3]} | xargs)
+				echo "..[BACKUP Debug: Setting TaskVers to $lTaskVers"
+				return;
+        	fi        
+    	done < ../tarefas-salvas/tarefas.txt
+    	IFS=';'
 }
 
 
@@ -61,19 +75,21 @@ function getTaskLineByTaskName(){
 #no servidor onde este script estiver rodando
 #essa pasta montada devera ter o nome 192.168.0.40DocsNSI
 #pra deixar bem claro a qual pasta compartilhada ela esta se referindo
-function extraiIpENomeDaPastaOrigem(){
-    origemRevertida=$(rev <<< $1);
-    echo $origemRevertida
-    IFS='/' #  (/) is set as delimiter
+function extraiIpENomeDaPastaOrigemByTaskName(){
+	echo "[BACKUP] Debug: function extraiIpENomeDaPastaOrigem(){..."
+	echo "[BACKUP] Debug: taskName = $1"
+	origemRevertida=$(rev <<< $1);
+    	echo "[BACKUP] Debug: $ origemRevertida=$origemRevertida"
+    	IFS='/' #  (/) is set as delimiter
 
-    read -ra ADDR <<< "$origemRevertida" # origemRevertida is read into an array as tokens separated by IFS
-    ipDeOrigem=$(rev <<< ${ADDR[1]})
-    pastaDeOrigem=$(rev <<< ${ADDR[0]})
+    	read -ra ADDR <<< "$origemRevertida" # origemRevertida is read into an array as tokens separated by IFS
+    	ipDeOrigem=$(rev <<< ${ADDR[1]})
+    	pastaDeOrigem=$(rev <<< ${ADDR[0]})
 
-    IFS=' ' # reset to default value after usage
+    	IFS=' ' # reset to default value after usage
 
-    echo [extraiIpENomeDaPastaOrigem] ipDeOrigem $ipDeOrigem
-    echo [extraiIpENomeDaPastaOrigem] pastaDeOrigem $pastaDeOrigem
+    	echo "[BACKUP] Debug: [extraiIpENomeDaPastaOrigem] ipDeOrigem $ipDeOrigem"
+    	echo "[BACKUP] Debug: [extraiIpENomeDaPastaOrigem] pastaDeOrigem $pastaDeOrigem"
 }
 
 #Todos os backups irao para pasta //192.168.0.150/Backup_IPGG
@@ -89,26 +105,26 @@ function extraiIpENomeDaPastaOrigem(){
 function montaPastaDeOrigem(){
     sharedNameDaPastaDeOrigem=$1 #algo do tipo //192.168.0.40/Docs_NSI
     pathDeMontagemParaOrigem=$2 #algo do tipo /home/$USER/mnt/192.168.0.40DocsNSI
-    echo "sharedNameDaPastaDeOrigem=$sharedNameDaPastaDeOrigem"
-    echo "pathDeMontagemParaOrigem=$pathDeMontagemParaOrigem"
+    echo "[BACKUP] Debug: sharedNameDaPastaDeOrigem=$sharedNameDaPastaDeOrigem"
+    echo "[BACKUP] Debug: pathDeMontagemParaOrigem=$pathDeMontagemParaOrigem"
     
     options="credentials=$mountCifsCredentialFile,file_mode=0777,dir_mode=0777,vers=$lTaskVers"    
     mkdir -p $pathDeMontagemParaOrigem
-    echo [montaPastaDeOrigem] mount -t cifs $sharedNameDaPastaDeOrigem $pathDeMontagemParaOrigem -o $options
-    echo "nicolas1" | sudo -S mount -t cifs $sharedNameDaPastaDeOrigem $pathDeMontagemParaOrigem -o $options
+    echo "[BACKUP] Debug: mount -t cifs  $sharedNameDaPastaDeOrigem  $pathDeMontagemParaOrigem -o $options"
+    echo "sessp@ipgg" | sudo -S mount -t cifs $sharedNameDaPastaDeOrigem $pathDeMontagemParaOrigem -o $options
 }
 
 
 
-
-getTaskLineByTaskName
+#getTaskLineByTaskName $nomeDaTarefa will test if nomeDaTarefa is founded
+getTaskLineByTaskName $nomeDaTarefa	
 if [ $nomeDaTarefaFounded -ne "1" ] ; 
    then
-       echo "??? Tarefa $nomeDaTarefa nao encontrada"
+       echo "[BACKUP] Err: Tarefa informada: $nomeDaTarefa nao encontrada"
        exit 1
 fi
 
-mountCifsCredentialFile="$(pwd)/domainUserCredentials"
+mountCifsCredentialFile="$(pwd)/domainUserCredentials" #pwd is chanced to basedir
 #testar se as variaveis foram criadas, caso contrario nao vale a pena continuar
 # a execucao do script
 # lTaskNameSelected
@@ -122,18 +138,26 @@ mountCifsCredentialFile="$(pwd)/domainUserCredentials"
 # dentro dessa pasta padrao mnt150Backup_IPGG
 #montaPastaPadraoDeDestidoDeTodosOsBackups 
 
-extraiIpENomeDaPastaOrigem $lTaskOrigem # cria as vars  $ipDeOrigem e $pastaDeOrigem
+extraiIpENomeDaPastaOrigemByTaskName $lTaskOrigem # cria as vars  $ipDeOrigem e $pastaDeOrigem
 
-montaPastaDeOrigem "//$ipDeOrigem/$pastaDeOrigem" "$basePathToMountOrigem/mnt/$pastaDeOrigem"
+montaPastaDeOrigem "//$ipDeOrigem/$pastaDeOrigem" "$basePathToMount_CIFS_Origem/$pastaDeOrigem"
 
 
 
-files=$(shopt -s nullglob dotglob; echo $basePathToMountOrigem/mnt/$pastaDeOrigem/*)
+files=$(shopt -s nullglob dotglob; echo $basePathToMountOrigem/$pastaDeOrigem/*)
 if (( ${#files} ))
 then
-    echo "contains files"
-    echo rsync -va $basePathToMountOrigem/mnt/$pastaDeOrigem admin@192.168.0.150:/admin@192.168.0.150:/share/Backup_IPGG/
-    rsync -ratlzv $basePathToMountOrigem/mnt/$pastaDeOrigem admin@192.168.0.150:/share/Backup_IPGG/
+    echo "[BACKUP] Existem arquivos a serem backapeados em $pastaDeOrigem"
+    if [ "X$pastaDeOrigem" == "X" ]
+    	then
+	    echo "[BACKUP] Err 5 pastaDeOrigem com valor : $pastaDeOrigem"
+	    exit 5
+    	else
+	    echo "[BACKUP] rsync -va $basePathToMountOrigem/$pastaDeOrigem admin@192.168.0.150:/share/Backup_IPGG/"
+	    echo "[BACKUP] ![$(date)] Inicio Backup $basePathToMountOrigem/$pastaDeOrigem" >> $HOME/backupserver.log
+	    #rsync -ratlzv $basePathToMountOrigem/$pastaDeOrigem admin@192.168.0.150:/share/Backup_IPGG/
+	    echo "[BACKUP] ![$(date)] Terminou Backup $basePathToMountOrigem/$pastaDeOrigem" >> $HOME/backupserver.log
+    fi;
 else 
   echo "empty (or does not exist or is a file)"
 fi
